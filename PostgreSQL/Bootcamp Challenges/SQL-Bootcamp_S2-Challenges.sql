feb90f139f173b68a061f395cd176fb444dd6aa3
Ashwin Chidambaram <ashwin.chidambaram@gmail.com>
2019-07-23 14:53:13 -0700

    Moving Files

---
+++ PostgreSQL/Bootcamp Challenges/SQL-Bootcamp_S2-Challenges.sql
@@ -0,0 +1,7 @@
+/* Challenge # 1: SELECT task
+
+Use a SELECT statement to grab the first and last names of every customer and their email addresses
+*/
+
+SELECT first_name, last_name, email
+FROM customer
---
+++ PostgreSQL/Bootcamp Notes/SQL-Bootcamp_S2-StatementFundamentals.sql
@@ -0,0 +1,13 @@
+/* SELECT Statements ########################################
+
+Basic syntax of a SELECT Statement:
+
+  SELECT column1, column2, ...
+  FROM  tableName
+
+Can also use "*" to select all columns from table
+
+EXAMPLE: */
+
+  SELECT first_name, last_name
+  FROM actor
