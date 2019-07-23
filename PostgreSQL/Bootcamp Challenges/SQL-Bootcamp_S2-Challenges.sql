c6be655339dd263638a4ea87bb3348f47cc95241
Ashwin Chidambaram <ashwin.chidambaram@gmail.com>
2019-07-23 15:35:00 -0700

    2.16

--- PostgreSQL/Bootcamp Challenges/SQL-Bootcamp_S2-Challenges.sql
+++ PostgreSQL/Bootcamp Challenges/SQL-Bootcamp_S2-Challenges.sql
@@ -1,7 +1,58 @@
+/*############################################################################*/
 /* Challenge # 1: SELECT task
 
-Use a SELECT statement to grab the first and last names of every customer and their email addresses
+Use a SELECT statement to grab the first and last names of every customer and
+their email addresses
 */
 
 SELECT first_name, last_name, email
 FROM customer
+
+
+/*############################################################################*/
+/* Challenge # 2: SELECT DISTINCT
+
+We want to know the types of ratings movies can get in the United States
+(e.g. PG, PG-13, R, etc.) and which ones we have in our database
+*/
+
+SELECT DISTINCT rating
+FROM film
+
+
+/*############################################################################*/
+/* Challenge # 3: SELECT WHERE (1)
+
+A customer forogt their wallet at our store! We need to track down their email
+to inform them. What's the email for the customer with the name Nancy Thomas?
+*/
+
+SELECT email
+FROM customer
+WHERE first_name = 'Nancy'AND last_name = 'Thomas'
+
+
+/* Challenge # 3: SELECT WHERE (2)
+
+A customer wants to know what the movie "Outlaw Hanky" is about. Could you give
+them the description for the movie "Outlaw Hanky"?
+*/
+
+SELECT description
+FROM film
+WHERE title = 'Outlaw Hanky'
+
+
+/* Challenge # 3: SELECT WHERE (3)
+
+A customer is late on their movie return. I know their address is "259 Ipoh
+Drive". I want to call them to let them know! Can you get me the phone number
+for the person who lives at "259 Ipoh Drive"?
+*/
+
+SELECT phone
+FROM address
+WHERE address = '259 Ipoh Drive'
+
+
+/*############################################################################*/
--- PostgreSQL/Bootcamp Notes/SQL-Bootcamp_S2-StatementFundamentals.sql
+++ PostgreSQL/Bootcamp Notes/SQL-Bootcamp_S2-StatementFundamentals.sql
@@ -1,13 +1,105 @@
-/* SELECT Statements ########################################
+/*############################################################################*/
+/* SELECT Statements
+
+Usage: Used to select columns from a table
 
 Basic syntax of a SELECT Statement:
 
   SELECT column1, column2, ...
   FROM  tableName
 
-Can also use "*" to select all columns from table
+Notes:
+  - Can also use "*" to select all columns from table
 
 EXAMPLE: */
 
   SELECT first_name, last_name
   FROM actor
+
+
+/*############################################################################*/
+/* SELECT DISTINCT Statements
+
+Usage: Used to select distinct values from a table
+
+Basic syntax of a SELECT DISTINCT Statement:
+
+  SELECT DISTINCT column1, column2, ...
+  FROM  tableName
+
+EXAMPLE: */
+
+  SELECT DISTINCT release_year
+  FROM film
+
+
+/*############################################################################*/
+/* WHERE Clause
+
+Usage: Used when we want to query just particular rows from a table or under
+  some condition/logic
+
+Basic syntax of a WHERE Clause:
+
+  SELECT column1, column2, ...
+  FROM tableName
+  WHERE condition
+
+Notes:
+  - Following are operators that can be used with WHERE clause:
+
+      =       Equal
+      >       Greater Than
+      <       Less Than
+      >=      Greater Than or Equal
+      <=      Less Than or Equal
+      <>      Not Equal
+      !=      Not Equal
+      AND     Logical Operator AND
+      OR      Logical Operator OR
+
+EXAMPLE: */
+
+  SELECT customer_id, amount, payment_date
+  FROM payment
+  WHERE amount <= 1 OR amount >= 8
+
+
+/*############################################################################*/
+/* 
+
+Usage:
+
+Basic syntax of :
+
+
+
+Notes:
+  -
+
+EXAMPLE: */
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+Usage:
+
+Basic syntax of :
+
+
+
+Notes:
+  -
+
+EXAMPLE: */
