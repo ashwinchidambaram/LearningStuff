3a995c33467442e30c8e34d1788bc9880c57330f
Ashwin Chidambaram <ashwin.chidambaram@gmail.com>
2019-07-23 20:03:17 -0700

    Finished Section 2, preped Section 3

--- PostgreSQL/Bootcamp Challenges/SQL-Bootcamp_S2-Challenges.sql
+++ PostgreSQL/Bootcamp Challenges/SQL-Bootcamp_S2-Challenges.sql
@@ -1,3 +1,7 @@
+/*########## SQL Bootcamp ####################################################*/
+/*############## Section 2: SQL Statement Fundamentals #######################*/
+/*######################### CHALLENGES #######################################*/
+
 /*############################################################################*/
 /* Challenge # 1: SELECT task
 
@@ -56,3 +60,79 @@ WHERE address = '259 Ipoh Drive'
 
 
 /*############################################################################*/
+/* Challenge # 4: ORDER BY (1)
+
+Get the customer ID numbers for the top 10 highest payment amounts
+*/
+
+SELECT customer_id
+FROM payment
+ORDER BY amount DESC
+
+
+/* Challenge # 4: ORDER BY (2)
+
+Get the titles of the movies with film IDs 1-5
+*/
+
+SELECT title
+FROM film
+WHERE film_id <= 5
+
+
+/*############################################################################*/
+/*###################### General Challenge # 1 ###############################*/
+/*############################################################################*/
+
+/* Challenge (1):
+
+How many payment transactions were greater than $5.00?
+*/
+
+SELECT COUNT(payment_id)
+FROM payment
+WHERE amount > 5
+
+/* Challenge (2):
+
+How many actors have a first name that starts with the letter P?
+*/
+
+SELECT COUNT(first_name)
+FROM actor
+WHERE first_name LIKE 'P%'
+
+/* Challenge (3):
+
+How many unique districts are our customers from?
+*/
+
+SELECT COUNT(DISTINCT(district))
+FROM address
+
+/* Challenge (4):
+
+Retrieve the list of names for those distinct districts from the previous
+question.
+*/
+
+SELECT DISTINCT(district)
+FROM address
+
+/* Challenge (5):
+
+How many films have a rating of R and a replacement cost between $5 and $15?
+*/
+
+SELECT COUNT(film_id)
+FROM film
+WHERE rating = 'R' AND replacement_cost BETWEEN 5 AND 15
+
+/* Challenge (6):
+
+How many films have the word Truman somewhere in the title?
+*/
+
+SELECT COUNT(film_id)
+FROM film
+WHERE title LIKE '%Truman%'
--- PostgreSQL/Bootcamp Notes/SQL-Bootcamp_S2-StatementFundamentals.sql
+++ PostgreSQL/Bootcamp Notes/SQL-Bootcamp_S2-StatementFundamentals.sql
@@ -1,3 +1,7 @@
+/*########## SQL Bootcamp ####################################################*/
+/*############## Section 2: SQL Statement Fundamentals #######################*/
+/*############################################################################*/
+
 /*############################################################################*/
 /* SELECT Statements
 
@@ -66,40 +70,172 @@ EXAMPLE: */
 
 
 /*############################################################################*/
-/* 
+/* COUNT Function
 
-Usage:
+Usage: To return the number of input rows that match a specific condition set
+  by the query
 
-Basic syntax of :
+Basic syntax of COUNT Function:
 
-
+  SELECT COUNT(columnName)
+  FROM tableName
 
 Notes:
-  -
+  - The COUNT function does not consider null values within column
+  - We can also use COUNT with DISTINCT
+
+      SELECT COUNT(DISTINCT columnName)
+      FROM table
 
 EXAMPLE: */
 
+  SELECT COUNT(*)
+  FROM payment
+__________
+
+  SELECT COUNT(DISTINCT (amount))
+  FROM payment
 
 
+/*############################################################################*/
+/* LIMIT Statement
+
+Usage: Allows you to limit the number of rows you get back from a query
+
+Basic syntax of LIMIT Statement:
+
+  SELECT (*)
+  FROM tableName
+  LIMIT 5
+
+EXAMPLE: */
+
+  SELECT *
+  FROM customer
+  LIMIT 5
 
 
+/*############################################################################*/
+/* ORDER BY Statements
+
+Usage: Allows us to sort rows returned from SELECT statement in ascending or
+  descending order
+
+Basic syntax of ORDER BY Statements:
+
+  SELECT column1, column2
+  FROM tableName
+  ORDER BY column1 ASC/DESC
+
+Notes:
+  - If left blank, the ORDER BY statement will choose ascending by default
+
+EXAMPLE: */
+
+  SELECT first_name, last_name
+  FROM customer
+  ORDER BY first_name ASC
 
 
+/*############################################################################*/
+/* BETWEEN Operator
+
+Usage: Allows us to match a value against a range of values
+
+Basic syntax of BETWEEN Operator:
+
+  someValue BETWEEN lowEnd AND highEnd
+
+Notes:
+  - Between operator also includes the boundries
+
+  - Can also be rewritten as:
+
+      someValue >= lowEnd AND someValue <= highEnd
+
+  - Can also use NOT with BETWEEN
+
+      someValue NOT BETWEEN lowEnd AND highEnd
+
+EXAMPLE: */
+
+  SELECT amount, payment_date
+  FROM payment
+  WHERE payment_date BETWEEN '2007-02-07' AND '2007-02-15'
+__________
+
+  SELECT customer_id, amount
+  FROM payment
+  WHERE amount NOT BETWEEN 8 AND 9
+
+/*############################################################################*/
+/* IN Statement
+
+Usage: Must be used with the WHERE clause to check if a value matches any value
+  in a list of values. The expression returns true of the value matches and
+  value in the list (i.e. value1, value2)
+
+Basic syntax of IN Statement:
+
+  someValue IN (value1, value2, ...)
+
+Notes:
+  - The list of values is not limited to a list of numbers or a string, but also
+    a result set of a SELECT statement. (Shown below)                           ||1||
+
+        someValue IN (SELECT someValue
+                      FROM tableName)
+
+  - Can also use a NOT statement to adjust an IN statement (NOT IN)
+
+EXAMPLE: */
+
+  SELECT customer_id, rental_id, return_date
+  FROM rental
+  WHERE customer_id IN (1,2)
+  ORDER BY return_date DESC
+
+/*############################################################################*/
+/* LIKE Statement
+
+Usage:
+
+Basic syntax of LIKE Statement:
+
+  SELECT column1, column2
+  FROM tableName
+  WHERE aColumn LIKE 'someString%'
+
+Notes:
+  - The '%' is a special symbol that is reffered to as a pattern. Essentially,
+    it will fill in the blank assuming some basis                               ||2||
+
+EXAMPLE: */
+
+  SELECT first_name, last_name
+  FROM customer
+  WHERE first_name LIKE 'Jen%'
+__________
+
+  SELECT first_name, last_name
+  FROM customer
+  WHERE first_name LIKE '%er%'
+__________
+
+  SELECT first_name, last_name
+  FROM customer
+  WHERE first_name LIKE '_her%'
 
 
-
-
-
-
+/*############################################################################*/
+/*######################## EXTRA NOTES #######################################*/
+/*############################################################################*/
 /*
+||1|| - This is a simple subquery
+||2|| - How to use a pattern (%)
 
-Usage:
+    You can construct a pattern by combining a string with wildcard characters
+    and use the LIKE or NOT LIKE operators to find the matches
 
-Basic syntax of :
-
-
-
-Notes:
-  -
-
-EXAMPLE: */
+      - Use the percent (%) for matching any sequence of characters
+      - Use the underscore (_) for matching any single character
