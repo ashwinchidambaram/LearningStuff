6f358c8edfb7e44e2475ef9bfb030580dd5475de
Ashwin Chidambaram <ashwin.chidambaram@gmail.com>
2019-08-08 11:30:18 -0700

    Setup notes doc for Section 8

---
+++ PostgreSQL/Bootcamp Notes/SQL-Bootcamp_S8-Creating_Databases_and_Tables.sql
@@ -0,0 +1,215 @@
+/*########## SQL Bootcamp ####################################################*/
+/*############## Section 8: Creating Databases and Tables ####################*/
+/*############################################################################*/
+
+/*############################################################################*/
+/* Data Types
+
+Usage:
+
+Basic syntax of :
+
+
+
+Notes:
+  -
+
+EXAMPLE: */
+
+
+
+/*############################################################################*/
+/* Primary Keys and Foreign Keys
+
+Usage:
+
+Basic syntax of :
+
+
+
+Notes:
+  -
+
+EXAMPLE: */
+
+
+
+/*############################################################################*/
+/* Create Table
+
+Usage:
+
+Basic syntax of :
+
+
+
+Notes:
+  -
+
+EXAMPLE: */
+
+
+
+/*############################################################################*/
+/* Insert
+
+Usage:
+
+Basic syntax of :
+
+
+
+Notes:
+  -
+
+EXAMPLE: */
+
+
+
+/*############################################################################*/
+/* Update
+
+Usage:
+
+Basic syntax of :
+
+
+
+Notes:
+  -
+
+EXAMPLE: */
+
+
+
+/*############################################################################*/
+/* Delete
+
+Usage:
+
+Basic syntax of :
+
+
+
+Notes:
+  -
+
+EXAMPLE: */
+
+
+
+/*############################################################################*/
+/* Alter Table
+
+Usage:
+
+Basic syntax of :
+
+
+
+Notes:
+  -
+
+EXAMPLE: */
+
+
+
+/*############################################################################*/
+/* Drop Table
+
+Usage:
+
+Basic syntax of :
+
+
+
+Notes:
+  -
+
+EXAMPLE: */
+
+
+
+/*############################################################################*/
+/* CHECK Constraint
+
+Usage:
+
+Basic syntax of :
+
+
+
+Notes:
+  -
+
+EXAMPLE: */
+
+
+
+/*############################################################################*/
+/* NOT NULL Constraint
+
+Usage:
+
+Basic syntax of :
+
+
+
+Notes:
+  -
+
+EXAMPLE: */
+
+
+
+/*############################################################################*/
+/* UNIQUE Constraint
+
+Usage:
+
+Basic syntax of :
+
+
+
+Notes:
+  -
+
+EXAMPLE: */
+
+
+
+/*############################################################################*/
+/*
+
+Usage:
+
+Basic syntax of :
+
+
+
+Notes:
+  -
+
+EXAMPLE: */
+
+
+
+/*############################################################################*/
+
+
+
+
+
+
+/*############################################################################*/
+/*######################## EXTRA NOTES #######################################*/
+/*############################################################################*/
+/*
+||1|| - This is a simple subquery
+||2|| - How to use a pattern (%)
+
+    You can construct a pattern by combining a string with wildcard characters
+    and use the LIKE or NOT LIKE operators to find the matches
+
+      - Use the percent (%) for matching any sequence of characters
+      - Use the underscore (_) for matching any single character
