dc4f4e8458cf4a16f0f2441e1e008b2439bd14ca
Ashwin Chidambaram <ashwin.chidambaram@gmail.com>
2019-08-12 12:22:43 -0700

    Finished Assessment Test 3 1/1

--- PostgreSQL/Bootcamp Assessment Tests/AssessmentTest3.sql
+++ PostgreSQL/Bootcamp Assessment Tests/AssessmentTest3.sql
@@ -31,14 +31,55 @@ Then insert a teacher names Jonas Salk (teacher_id = 1) who as a homeroom number
 
 */
 
+-- Creating 'teachers' table
+CREATE TABLE teachers(
+	teacher_id serial PRIMARY KEY,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	homeroom_number integer NOT NULL,
+	department varchar(50) NOT NULL,
+	email varchar(350) UNIQUE NOT NULL,
+	phone varchar(10) UNIQUE NOT NULL)
+
+-- Creating 'students' table
+CREATE TABLE students(
+	student_id serial PRIMARY KEY,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	homeroom_number integer NOT NULL,
+	phone varchar(10) UNIQUE NOT NULL,
+	email varchar(350) UNIQUE NOT NULL,
+	grad_year integer NOT NULL)
+
+-- Inserting one student
+INSERT INTO students(student_id, first_name, last_name, homeroom_number, phone, email, grad_year)
+VALUES(1, 'Mark', 'Watney', 5, 7775551234, 'NA', 2035)
+
+-- Inserting one teacher
+INSERT INTO teachers(teacher_id, first_name, last_name, homeroom_number, department, email, phone)
+VALUES(1, 'Jonas', 'Salk', 5, 'Biology', 'jsalk@school.org', 7775554321)
 
 
 /* RESULT: CORRECT
 
-select *
-from cd.facilities;
+CREATE TABLE students(
+student_id serial PRIMARY KEY,
+first_name VARCHAR(45) NOT NULL,
+last_name VARCHAR(45) NOT NULL,
+homeroom_number integer,
+phone VARCHAR(20) UNIQUE NOT NULL,
+email VARCHAR(115) UNIQUE,
+grad_year integer);
 
-*/
+CREATE TABLE teachers(
+teacher_id serial PRIMARY KEY,
+first_name VARCHAR(45) NOT NULL,
+last_name VARCHAR(45) NOT NULL,
+homeroom_number integer,
+department VARCHAR(45),
+email VARCHAR(20) UNIQUE,
+phone VARCHAR(20) UNIQUE);
 
+INSERT INTO students(first_name,last_name, homeroom_number,phone,grad_year)VALUES ('Mark','Watney',5,'7755551234',2035);
 
-/*############################################################################*/
+INSERT INTO teachers(first_name,last_name, homeroom_number,department,email,phone)VALUES ('Jonas','Salk',5,'Biology','jsalk@school.org','7755554321');
