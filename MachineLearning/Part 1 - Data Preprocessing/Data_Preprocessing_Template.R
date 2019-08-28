2f72924287999399bfef9325bac2785d61bbb1da
Ashwin Chidambaram <ashwin.chidambaram@gmail.com>
2019-08-27 19:37:34 -0700

    Finished 2.19

--- MachineLearning/Part 1 - Data Preprocessing/Data_Preprocessing_Template.R
+++ MachineLearning/Part 1 - Data Preprocessing/Data_Preprocessing_Template.R
@@ -2,3 +2,20 @@
 
 # Importing the Dataset
 dataset = read.csv('Data.csv')
+
+# Taking Care of the Missing Data
+dataset$Age = ifelse(is.na(dataset$Age), 
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)), 
+                     dataset$Age) 
+  # Code Breakdown: 
+    # What it's doing: Replacing the missing value in the column age by the mean of the column age itself
+    # By using '$' after the dataset name, we can select the column of interest 
+    # dataset$Age                                                     => Modifying 'Age' column of dataset 
+    # ifelse                                                          => Syntax: (test, condition TRUE , condition FALSE)
+    # is.na(dataset$Age)                                              => If there exists NaN field in 'Age' column of dataset 
+    # ave(dataset$Age, FUN = function(x) mean (x, na.rm = TRUE))      => Compute average of which column of dataset, add function 
+    # dataset$Age)                                                    => If value in column is not missing, return original 
+
+dataset$Salary = ifelse(is.na(dataset$Salary), 
+                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)), 
+                     dataset$Salary) 
--- MachineLearning/Part 1 - Data Preprocessing/Data_Preprocessing_Template.py
+++ MachineLearning/Part 1 - Data Preprocessing/Data_Preprocessing_Template.py
@@ -27,3 +27,20 @@ X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, 3].values
     ## By using the first ':', we take all of the rows in the dataset
     ## By using the second '3', we are taking the third column (which is dependent in this case)
+
+# Taking Care of Missing Data 
+from sklearn.impute import SimpleImputer 
+    ## sklearn AKA 'scikit learn' which has the ability to preprocess data
+    ## Using SimpleImputer class which handles missing data
+    
+# NOTE: By highlighting a keyword and presssing 'Command I', we can inspect and understand syntax/usage 
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+    ## First we create the ruleset by which imputer will work 
+
+imputer = imputer.fit(X[:, 1:3])
+    ## Using imputer, we fit the X dataset in range [:, 1:3] and set it to the imputer object
+        ### The ':' represents all lines/rows as before 
+        ### The '1:3' represents the rows in which data is missing (1,2) but we use 3 as the end becuase the upper bound is always excluded
+
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+    ## We are applying the method of the imputer object 'transform' that replaces the missing data with the mean of the column
